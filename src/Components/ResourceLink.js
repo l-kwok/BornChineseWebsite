c19c325fdeb63e0769059dec32935cd066ce5665
Linden Kwok <lindenstkwok@gmail.com>
2021-04-23 01:30:58 -0700

    Added Cards Resource Links

---
+++ src/Components/ResourceLink.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "../Styles/ResourceLink.css";
+
+const ResourceLink = ({ title, body, link }) => {
+	return (
+		<Card>
+			<Card.Body>
+				<Card.Title>{title}</Card.Title>
+				<Card.Text>{body}</Card.Text>
+				<Card.Link href={link}>
+					<Button variant="outline-info" className="learnMore-Button">Learn More</Button>
+				</Card.Link>
+			</Card.Body>
+		</Card>
+	);
+};
+
+export default ResourceLink;
